886428e0c377e69ab7abaf53e4ad5f46e73f8ab0
jonnybrazil <jon1444@gmail.com>
2026-09-13 22:04:05 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Apenas permite que o site funcione normalmente e processe os fetches de CSV/áudio
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
